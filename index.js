666a19ecc3494079f9bd05faaa16acb3036436d3
Crown Cao <crown_0726@163.com>
2017-03-19 21:24:55 +0800

    Enhance the program.

--- index.js
+++ index.js
@@ -4,8 +4,24 @@
     var env = require('jsdom').env;
     var fs = require('fs');
     var _ = require('lodash');
-    // Read website resource
+
+    // Read website resource & initial data ================================================================================================
     var html = 'http://www.espn.com/nba/playbyplay?gameId=400900372';
+    var selectedQuarter = 'gp-quarter-1';
+    var initMemeber = ['Richard Jefferson', 'LeBron James', 'Tristan Thompson', 'Kyrie Irving', 'Iman Shumpert',
+        'Aaron Gordon', 'Terrence Ross', 'Nikola Vucevic', 'Elfrid Payton', 'Evan Fournier'];
+    var cavaliersMembers = ['LeBron James', 'Kyrie Irving', 'Kevin Love', 'Kyle Korver', 'Channing Frye', 'J.R. Smith', 'Derrick Williams',
+        'Tristan Thompson', 'Iman Shumpert', 'Deron Williams', 'Richard Jefferson', 'Mike Dunleavy', 'Jordan McRae', 'Kay Felder',
+        'James Jones', 'DeAndre Liggins', 'Chris Andersen', 'Andrew Bogut', 'Larry Sanders'];
+    var magicMembers = ['Evan Fournier', 'Serge Ibaka', 'Nikola Vucevic', 'Elfrid Payton', 'Aaron Gordon', 'Terrence Ross', 'Jeff Green',
+        'Jodie Meeks', 'D.J. Augustin', 'Bismack Biyombo', 'C.J. Watson', 'Mario Hezonja', 'Anthony Brown', 'Damjan Rudez', 'Stephen Zimmerman',
+        'CJ Wilcox', 'Arinze Onuaku'];
+    var actions = ['two point shot', 'jumper', 'free throw 1 of 2', 'free throw 2 of 2', 'free throw 1 of 3',
+        'free throw 2 of 3', 'free throw 3 of 3', 'driving layup', 'layup', 'shot', 'tip shot', 'jumpshot', 'finger roll layup',
+        'hook shot', 'three point jumper', 'three point shot', 'defensive rebound', 'offensive rebound', 'defensive team rebound',
+        'offensive team rebound', 'assists', 'traveling', 'blocks', 'official timeout', 'full timeout', 'offensive Charge',
+        'shooting foul', 'personal foul', 'shooting block foul', 'lost ball', 'bad pass', 'steals'];
+    // Read website resource & initial data ================================================================================================
 
     // first argument can be html string, filename, or url
     env(html, function (errors, window) {
@@ -13,56 +29,41 @@
             console.log(errors);
 
         var $ = require('jquery')(window);
+        var gpQquarter = $('#' + selectedQuarter + '').find('tbody tr');
 
-        // console.log($('#gp-quarter-1').find('tbody tr'));
         var dataStr = '';
         var dataLine = '';
-        var gpQquarter1 = $('#gp-quarter-1').find('tbody tr');
-        var initMemeber = ['Richard Jefferson', 'LeBron James', 'Tristan Thompson', 'Kyrie Irving', 'Iman Shumpert',
-            'Aaron Gordon', 'Terrence Ross', 'Nikola Vucevic', 'Elfrid Payton', 'Evan Fournier'];
 
-        var cavaliersMembers = ['LeBron James', 'Kyrie Irving', 'Kevin Love', 'Kyle Korver', 'Channing Frye', 'J.R. Smith', 'Derrick Williams',
-            'Tristan Thompson', 'Iman Shumpert', 'Deron Williams', 'Richard Jefferson', 'Mike Dunleavy', 'Jordan McRae', 'Kay Felder',
-            'James Jones', 'DeAndre Liggins', 'Chris Andersen', 'Andrew Bogut', 'Larry Sanders'];
-        var magicMembers = ['Evan Fournier', 'Serge Ibaka', 'Nikola Vucevic', 'Elfrid Payton', 'Aaron Gordon', 'Terrence Ross', 'Jeff Green',
-            'Jodie Meeks', 'D.J. Augustin', 'Bismack Biyombo', 'C.J. Watson', 'Mario Hezonja', 'Anthony Brown', 'Damjan Rudez', 'Stephen Zimmerman',
-            'CJ Wilcox', 'Arinze Onuaku'];
-        var actions = ['two point shot', 'jumper', 'free throw 1 of 2', 'free throw 2 of 2', 'free throw 1 of 3',
-            'free throw 2 of 3', 'free throw 3 of 3', 'driving layup', 'layup', 'shot', 'tip shot', 'jumpshot', 'finger roll layup',
-            'hook shot', 'three point jumper', 'three point shot', 'defensive rebound', 'offensive rebound', 'defensive team rebound',
-            'offensive team rebound', 'assists', 'traveling', 'blocks', 'official timeout', 'full timeout', 'offensive Charge',
-            'shooting foul', 'personal foul', 'shooting block foul', 'lost ball', 'bad pass', 'steals'];
-
-        for (var i = 0; i < gpQquarter1.length; i++) {
+        for (var i = 0; i < gpQquarter.length; i++) {
+            var dataArr = $(gpQquarter[i]).find('td'); // All data will store to this array
             // 1. Add moment
-            dataLine += $($(gpQquarter1[i]).find('td')[0]).html() + ';';
+            dataLine += $(dataArr[0]).html() + ';';
 
-
-            if ($($(gpQquarter1[i]).find('td')[1]).find('img').attr('src').indexOf('orl') > -1) {
+            if ($(dataArr[1]).find('img').attr('src').indexOf('orl') > -1) {
                 // 2. Add actions
-                dataLine += actionsFilter(actions, $($(gpQquarter1[i]).find('td')[2]).html()) + ';';
+                dataLine += actionsFilter(actions, $(dataArr[2]).html()) + ';';
                 // 3. Add the certain action actor = player
-                dataLine += playersFilter(magicMembers, $($(gpQquarter1[i]).find('td')[2]).html()) + ';';
+                dataLine += playersFilter(magicMembers, $(dataArr[2]).html()) + ';';
                 // 4. Add player home
                 dataLine += 'Magic' + ';';
                 // 5. Add score
-                dataLine += $($(gpQquarter1[i]).find('td')[3]).html() + ';';
+                dataLine += $(dataArr[3]).html() + ';';
             }
 
-            if ($($(gpQquarter1[i]).find('td')[1]).find('img').attr('src').indexOf('cle') > -1) {
+            if ($(dataArr[1]).find('img').attr('src').indexOf('cle') > -1) {
                 // 2. Add actions
-                dataLine += actionsFilter(actions, $($(gpQquarter1[i]).find('td')[2]).html()) + ';';
+                dataLine += actionsFilter(actions, $(dataArr[2]).html()) + ';';
                 // 3. Add the certain action actor = player
-                dataLine += playersFilter(cavaliersMembers, $($(gpQquarter1[i]).find('td')[2]).html()) + ';';
+                dataLine += playersFilter(cavaliersMembers, $(dataArr[2]).html()) + ';';
                 // 4. Add player home
                 dataLine += 'Cavaliers' + ';';
                 // 5. Add score
-                dataLine += $($(gpQquarter1[i]).find('td')[3]).html() + ';';
+                dataLine += $(dataArr[3]).html() + ';';
             }
 
             // 6. Add players who on the ground
-            if ($($(gpQquarter1[i]).find('td')[2]).html().indexOf('enters the game for') > -1) {
-                var memberTempArr = _.split($($(gpQquarter1[i]).find('td')[2]).html(), 'enters the game for', 2);
+            if ($(dataArr[2]).html().indexOf('enters the game for') > -1) {
+                var memberTempArr = _.split($(dataArr[2]).html(), 'enters the game for', 2);
                 for (var j = 0; j < initMemeber.length; j++) {
                     if (initMemeber[j] == _.trim(memberTempArr[1]))
                         initMemeber[j] = _.trim(memberTempArr[0])
@@ -81,6 +82,7 @@
         // console.log(dataStr);
         console.log('data========================================================ending');
 
+        // Write to file
         fs.writeFile('data.csv', dataStr, (err) => {
             if (err) throw err;
             console.log('It\'s saved!');
